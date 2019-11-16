93d48477db17ec17cf2b91010ca144133ae81983
Calvin McDowell <calvinmac40@hotmail.com>
2019-11-16 20:39:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
-
-end
+# def oxford_comma(array)
+#   if array.size == 1 
+#     array.join
+#   elsif array.length == 2 
+#   array.join(" and ")
+#   else array.length > 2 
+#   array[-1].prepend "and "
+#   array.join(", ")
+# end
+# end
